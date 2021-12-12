85212495b212916d1e57f97d159bb9ab83e34d9e
“sashininliyanage” <nimthara.liyanage@gmail.com>
2021-12-12 12:58:55 +0530

    add photo storage

--- api/index.js
+++ api/index.js
@@ -6,12 +6,28 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/category');
+const multer = require('multer');
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(console.log("Connected to MongoDB"))
 .catch((error) => console.log(error));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"images");
+    },
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name)
+    }
+});
+
+const upload = multer({storage:storage})
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    res.status(200).json("File has been uploaded");
+})
+
 
 app.use(express.json());
 app.use("/api/auth",authRoute);
